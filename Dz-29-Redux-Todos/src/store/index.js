2294600e18c15cee3b9e889ae5c1352bb8d1cc8d
Dmitriy <50639080+demos1981@users.noreply.github.com>
2023-01-16 11:03:26 +0200

    Delete Dz-29-Redux-Todos directory

--- Dz-29-Redux-Todos/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import {createStore} from 'redux';
-import todosReducer from './reducers/todosReducer';
-
-const store = createStore(todosReducer);
-export default store;
--- Dz-29-Redux-Todos/src/store/selectors/todos.js
+++
@@ -1,4 +0,0 @@
-export function selectTodos({todos}){
-    return todos;
-    
-}
